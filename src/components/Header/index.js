c210a0104f31e414516963ad66d38b7fabd6296c
hunter <hunter>
2022-03-02 15:46:46 -0300

    change header color on scroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,20 @@
 import './styles.css'
 import NavBar from '../NavBar';
+import { useState } from 'react';
 function Header(){
+    const [colorChange,setColorchange] = useState(false)
+    const changeNavbarColor = () =>{
+        if(window.scrollY >= 80){
+          setColorchange(true);
+        }
+        else{
+          setColorchange(false);
+        }
+     };
+     window.addEventListener('scroll', changeNavbarColor);
     return(
-        <header className="header">
+        <header className={colorChange? 'header not-transparent':'header'}
+        >
             <div className="logo">
                 <p>
                     Felipe <span>Baloneker</span>
