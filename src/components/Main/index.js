e213674736604f1049917a04ed4d3e03f9467fa8
hunter <hunter>
2022-03-02 15:19:07 -0300

    update home page

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,5 @@
 import './styles.css'
-
+import Typing from 'react-typing-animation';
 function Main(){
     return(
         <main>
@@ -8,7 +8,15 @@ function Main(){
                 <img src={require('../../assets/images/photo.jpeg')} alt="Foto" />
                 <h2>Felipe Baloneker</h2>
                 <h3><span>Desenvolvedor Web</span></h3>
-                <p>Criação de Aplicações FrontEnd</p>
+                <Typing speed={60}>
+                    <p>
+                        Desenvolvimento de Web Sites
+                        <Typing.Backspace count={40}/>
+                        Criação de Aplicações BackEnd
+                        <Typing.Backspace count={40}/>
+                        Criação de Aplicações FrontEnd
+                    </p>
+                </Typing>
             </div>
         </section>
       </main>
