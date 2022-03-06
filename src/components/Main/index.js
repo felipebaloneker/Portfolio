4cd6fc2107fd7a22acf66b90e15af22c126513f6
hunter <hunter>
2022-03-06 14:19:56 -0300

    updated projetos

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -139,22 +139,36 @@ function Main(){
                         <Projeto
                         link={"https://github.com/felipebaloneker/Chat-ReactJs"}
                         image={require('../../assets/images/chatJs.png')}
-                        title={'Projeto'}
-                        text={'Projeto text'}
+                        title={'Chat ReactJS'}
+                        text={'Chat em tempo real onde usuarios podem interagir entre si. Utiliza ReactJs com Typescript e css para o FrontEnd tem seu proprio BackEnd em NodeJs e Typeorm onde as mensagens são salvas.'}
                         />
                         <Projeto
-                        link={"https://github.com/felipebaloneker/Tarefas_React"}
-                        image={require('../../assets/images/chatJs.png')}
-                        title={'Projeto'}
-                        text={'Projeto text'}
+                        link={"https://github.com/felipebaloneker/MusicPlayer-ReactNative"}
+                        image={require('../../assets/images/musicPlayer.gif')}
+                        title={'Music Player'}
+                        text={'Um player de musica em React Native que toca arquivos em formato de audio salvos no smartphone.'}
+                        right
+                        />
+                        <Projeto
+                        link={"https://github.com/felipebaloneker/TelaLogin-Firebase"}
+                        image={require('../../assets/images/login.gif')}
+                        title={'Tela de Login'}
+                        text={'Um app FrontEnd em ReactJs que utiliza a autenticação do firebase como BackEnd.'}
+                        />
+                        <Projeto
+                        link={"https://github.com/felipebaloneker/ConversorMoedas-Vuejs"}
+                        image={require('../../assets/images/conversor.gif')}
+                        title={'Conversor de moedas'}
+                        text={'Um app FrontEnd em VueJs que consome Api de conversão de moedas.'}
                         right
                         />
                         <Projeto
                         link={"https://github.com/felipebaloneker/Tarefas_React"}
                         image={require('../../assets/images/tarefas.png')}
-                        title={'Projeto'}
-                        text={'Projeto text'}
+                        title={'Tarefas com react'}
+                        text={'Uma to do List onde usuario pode criar tarefas, marca-las como concluida e visualizar suas informações.'}
                         />
+
                 </div>
             </div>
         </section>
